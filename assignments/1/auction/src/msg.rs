3c1e273b1e36c513fcdb4847a0e69b2838f8cead
Kien Mai <imkevin@MacBook-Air.hitronhub.home>
2022-10-22 23:59:34 -0500

    Add messages msg.rs

--- assignments/1/auction/src/msg.rs
+++ assignments/1/auction/src/msg.rs
@@ -1,11 +1,66 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub ownder: Option<String>,
+}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    NewResource {
+        seller_id: Option<String>,
+        resource_id: Option<String>,
+        volume: f64,
+        price: f64,
+    },
+
+    PlaceBid {
+        resource_id: String,
+        buyer_id: Option<String>,
+        price: f64,
+    },
+    
+    CancelResource {
+        resource_id: String,
+    },
+
+    StartBidding {
+        resource_id: String,
+    },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+
+    #[returns(QueryMeritResponse)]
+    QueryMerit {},
+
+    #[returns(QueryResourceResponse)]
+    QueryResource {
+        resource_id: String,
+    },
+
+    #[returns(QueryBidsResponse)]
+    QueryBids {
+        resource_id: String,
+    },
+}
+
+#[cw_serde]
+pub struct QueryMeritResponse {
+    pub resources: Vec<Resource>,
+}
+
+#[cw_serde]
+pub struct QueryResourceResponse {
+    pub resource: Resource,
+    pub highest_bid: Option<Bid>,
+}
+
+#[cw_serde]
+pub struct QueryBidsResponse {
+    pub bids: Vec<Bid>,
+}
+
+
